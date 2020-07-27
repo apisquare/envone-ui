c9b6f7e5cc8dd3791da3b140dc2ec379669c5b5d
suthagar23 <suthagar.14@cse.mrt.ac.lk>
2020-07-27 21:11:12 +0530

    Add example with envone

---
+++ example/use_with_envone/index.js
@@ -0,0 +1,35 @@
+
+const express = require('express');
+const envOne = require('envone');
+const bodyParser = require("body-parser");
+const envOneUI = require('../../index');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Configure environment variables
+const configuredEnv = envOne.config();
+
+// Configure envOneUI
+app.use(
+  envOneUI.configure({
+    secrets: ['AWS_ACCESS_SECRET', 'DB_CONNECTION_PASSWORD'],
+    exclude: ['ANALYTICS_URL'],
+    include: ['ENV'],
+    isAuthRequired: true,
+    configOutput: configuredEnv
+  }),
+);
+
+app.get('/*', (req, res) => {
+  const response = `<h1 style='text-align: center'> Go to <a href='/env'> /env </a> to check your environment variables </h1>`;
+  res.send(response);
+});
+
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Listening on http://0.0.0.0:${port}`);
+});
